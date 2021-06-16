a9e433345c9c8561e8055312c1a29ec73e5e9b42
1996-isuru <isurusrimalnt@gmail.com>
2021-06-16 14:56:55 +0530

    solve the error in custom font

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from 'expo-font';
 
 import { Home, Restaurant, OrderDelivery } from "./screens";
 import Tabs from './navigation/tabs';
@@ -8,6 +9,18 @@ import Tabs from './navigation/tabs';
 const Stack = createStackNavigator();
 
 const App = () => {
+
+  const [loaded] = useFonts({
+    "Roboto-Black" : require('./assets/fonts/Roboto-Black.ttf'),
+    "Roboto-Bold" : require('./assets/fonts/Roboto-Bold.ttf'),
+    "Roboto-Regular" : require('./assets/fonts/Roboto-Regular.ttf'),
+
+  })
+
+  if(!loaded){
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -21,7 +34,7 @@ const App = () => {
       <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
-};
+  )
+}
 
 export default App;
