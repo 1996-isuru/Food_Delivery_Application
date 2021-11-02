185bf5d59b97a160ffb0f260fd3ada7ab11ce506
Isuru Srimal <57609424+1996-isuru@users.noreply.github.com>
2021-11-02 11:12:33 +0530

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -10,7 +10,7 @@ import {
 } from "react-native";
 import { COLORS, icons, images, SIZES, FONTS } from "../constants";
 
-//dummy data startrrrrrrrrwe
+//dummy data startrrrrrrrrweooo
 const initialCurrentLocation = {
   streetName: "Kuching",
   gps: {
